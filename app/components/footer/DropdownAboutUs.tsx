183903a570b2ab7bba5b81636bcab0ad65be39f0
Mikey <pride81_@hotmail.com>
2025-02-10 21:35:52 +0900

    Dropdown animation for sub menus added

--- app/components/footer/DropdownAboutUs.tsx
+++ app/components/footer/DropdownAboutUs.tsx
@@ -1,41 +1,91 @@
 "use client";
 
 import { aboutUsData } from "@/app/data/footer/data";
-import { MainMenus } from "@/app/types/footer/types";
+import { MainMenus, SubMenus } from "@/app/types/footer/types";
 import { Box, Typography } from "@mui/material";
 import React, { useState } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const SubMenuItem = ({
+  items,
+  menuOpen,
+}: {
+  items: SubMenus[];
+  menuOpen: boolean;
+}) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {items.map((item) => (
+        <Typography
+          sx={{
+            maxHeight: menuOpen ? "50px" : "0px",
+            overflow: "hidden",
+            transition: "max-height 0.2s ease-in-out",
+            // transition: {
+            //   "maxHeight: 0.2s ease-in-out,
+            // },
+            color: "rgba(0,0,0,.58)",
+            fontSize: "13px",
+            // paddingBottom: "30px",
+            // "&:hover": {
+            //   color: "#695844",
+            //   fontWeight: 550,
+            // },
+            // transition: "text-shadow 0.1s ease",
+            // paddingY: menuOpen && "2px",
+            "&:hover": {
+              textShadow: "0 0 1px currentColor",
+            },
+          }}
+          key={item.title}
+        >
+          {item.title}
+        </Typography>
+      ))}
+    </Box>
+  );
+};
 
 const MainTitle = ({ data }: { data: MainMenus }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <Box
-      display="flex"
-      alignItems="start"
-      justifyContent="space-between"
-      onClick={() => setMenuOpen(!menuOpen)}
-      sx={{
-        cursor: "pointer",
-      }}
-    >
-      <Typography
+    <Box>
+      <Box
+        display="flex"
+        alignItems="start"
+        justifyContent="space-between"
+        onClick={() => setMenuOpen(!menuOpen)}
         sx={{
-          fontSize: "14px",
-          fontWeight: 560,
-          letterSpacing: 1,
-          color: "#695844",
-          marginBottom: 3,
+          cursor: "pointer",
         }}
       >
-        {data.mainTitle}
-      </Typography>
-      <KeyboardArrowDownIcon
-        sx={{
-          transform: `rotate(${menuOpen ? "180deg" : "0deg"})`,
-          transition: "transform 0.5s ease-in-out",
-        }}
-      />
+        <Typography
+          sx={{
+            fontSize: "14px",
+            fontWeight: 560,
+            letterSpacing: 1,
+            color: "#695844",
+            marginBottom: 3,
+          }}
+        >
+          {data.mainTitle}
+        </Typography>
+        <KeyboardArrowDownIcon
+          sx={{
+            transform: `rotate(${menuOpen ? "180deg" : "0deg"})`,
+            transition: "transform 0.2s ease-in-out",
+          }}
+        />
+      </Box>
+      {/* {menuOpen && <SubMenuItem items={data.subMenus} menuOpen={menuOpen} />} */}
+
+      <SubMenuItem items={data.subMenus} menuOpen={menuOpen} />
     </Box>
   );
 };
