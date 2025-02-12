72f0da5db3a8aac919e00adb7e34562ee899fc40
Mikey <pride81_@hotmail.com>
2025-02-12 16:20:52 +0900

    polished account create page

--- app/account/create/page.tsx
+++ app/account/create/page.tsx
@@ -5,16 +5,49 @@ import React from "react";
 const AccountCreatePage = () => {
   return (
     <Box>
-      <Typography sx={{ fontWeight: 600, fontSize: "28px" }}>
-        Create an account
-      </Typography>
-      <Box>
-        <Typography>Starbucks® Rewards</Typography>
-        <Typography>
-          Join Starbucks Rewards to earn Stars for free food and drinks, any way
-          you pay. Get access to mobile ordering, a birthday Reward, and
-          moremore.
+      <Box
+        sx={{
+          maxWidth: 400,
+          mx: "auto",
+          mt: 4,
+          p: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 5,
+        }}
+      >
+        <Typography sx={{ fontWeight: 600, fontSize: "28px" }}>
+          Create an account
         </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: 3,
+            color: "#636c7b",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "14px",
+              fontWeight: 700,
+              textTransform: "uppercase",
+            }}
+          >
+            Starbucks® Rewards
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "14px",
+            }}
+          >
+            Join Starbucks Rewards to earn Stars for free food and drinks, any
+            way you pay. Get access to mobile ordering, a birthday Reward, and
+            moremore.
+          </Typography>
+        </Box>
       </Box>
       <AccountCreateForm />
     </Box>
